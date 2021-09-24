02513907d2a83c250abb70100a91339ab3cbe0fd
Mohammed Musthafa <64773619+mohdmusthafa@users.noreply.github.com>
2021-09-24 14:14:36 +0530

    Minor css fix  🤕

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="main-bg h-screen w-screen">
+  <div class="main-bg h-screen">
     <div class="container mx-auto flex items-center justify-center">
       <Resume />
     </div>
